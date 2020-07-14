a19c9363b559e36f6a2d39b3a7efb37f409f03de
DevBensmaine <bensmaine.hassan@gmail.com>
2020-07-15 00:50:09 +0100

    Add to card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Good } from '../../interfaces/good.interface'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  goods: Good[] = [
+    {name : 'appel', price: 5, photoUrl:'assets/appel.png'},
+    {name : 'Banane', price: 2, photoUrl:'assets/banane.png'},
+    {name : 'mango', price: 5, photoUrl:'assets/mango.jpg'},
+    {name : 'stroberry', price: 3, photoUrl:'assets/stroberry.jpg'},
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addToCard(index){
+      console.log("add - ",this.goods[index])
   }
 
 }
